9759929c3e2a8ec364ea04965e2a61d2cb595d8a
CHEREF-Mehdi <cheref-mehdi@hotmail.fr>
2020-09-12 19:42:43 +0200

    update .env & change docker file

--- ayomiUser/settings.py
+++ ayomiUser/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a4t0hh&*+5b#k7#d(&6+qu95%p+kse#1*6fkdyarf8%9o)k-lu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG can be True/False or 1/0
-DEBUG = int(os.environ.get('DEBUG', default=1))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "bootstrap4",
 ]
 
 MIDDLEWARE = [
